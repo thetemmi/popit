b0ecc53453b5e18511170a89357c8f2b423c50eb
thetemmi <122259937+thetemmi@users.noreply.github.com>
2023-11-13 21:04:51 +0200

    Add files via upload

---
+++ pages/constants/languages.ts
@@ -0,0 +1,19 @@
+export const languages = ['en', 'fr', 'ptbr', 'ru', 'es', 'tr'];
+export const languagesDisplay = ['en', 'fr', 'pt-br', 'ru', 'es', 'tr'];
+export const languagesModules = [
+  'common',
+  'infos',
+  'converter',
+  'parameters',
+  'overlay',
+  'captcha',
+  'problem',
+  'stats',
+  'chat',
+  'auth',
+  'notification',
+  'alerts',
+  'bookmark',
+  'faction',
+  'canvas'
+];
